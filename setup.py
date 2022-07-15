0cb725e8b125397a174f44c7e7a5d2aada040103
Steve Kieffer <skieffer@alpinemath.org>
2022-07-15 08:25:29 -0400

    Version 0.22.7

--- setup.py
+++ setup.py
@@ -1,17 +1,21 @@
 import setuptools
 
 with open("README.md", "r") as fh:
-    long_description = fh.read()
+    long_description = ''.join(fh.readlines()[1:])
 
 setuptools.setup(
     name="displaylang",
-    version="0.22.7-pre",
+    version="0.22.7",
     license="Apache 2.0",
     url="https://github.com/alpinemath/displaylang",
     description="Just enough Python to write displays",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'pfsc-util>=0.22.7',
+        'typeguard==2.13.3',
+    ],
     classifiers=[
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
